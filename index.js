f58da012e7d46cb8a368fde3fb65c2a71294eae9
AntonyHaro <antonyhpro@outlook.com>
2024-10-04 12:25:40 -0300

    simplificação do uso da api viacep

--- index.js
+++ index.js
@@ -11,6 +11,26 @@ app.get("/test", (req, res) => {
     res.json({ message: "Hello from the server!" });
 });
 
+// app.get("/cep/:cep", async (req, res) => {
+//     const { cep } = req.params;
+//     const url = `https://viacep.com.br/ws/${cep}/json/`;
+
+//     try {
+//         const response = await axios.get(url);
+//         const data = response.data;
+
+//         if (data.erro) {
+//             return res.status(404).json({ error: "CEP não encontrado." });
+//         }
+//         res.json(data);
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).json({
+//             error: "Erro ao buscar o CEP. Tente novamente mais tarde.",
+//         });
+//     }
+// });
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,47 @@
-const form = document.querySelector("form");
+const logradouroArea = document.getElementById("logradouro-area");
+const bairroArea = document.getElementById("bairro-area");
+const localidadeArea = document.getElementById("localidade-area");
+const estadoArea = document.getElementById("estado-area");
+const regiaoArea = document.getElementById("regiao-area");
+const ibgeArea = document.getElementById("ibge-area");
+const dddArea = document.getElementById("ddd-area");
+const ufArea = document.getElementById("uf-area");
 
-async function handleSearch(event) {
-    event.preventDefault();
+// function to fetch the cep data
+async function fetchCep(cep) {
     try {
-        const response = await fetch("/test");
+        const url = `https://viacep.com.br/ws/${cep}/json/`;
+        const response = await fetch(url);
+
+        if (!response.ok) throw new Error("Erro na solicitação");
+
         const data = await response.json();
-        window.alert(data.message);
+        return data.erro ? null : data;
     } catch (error) {
-        console.error("Erro:", error);
+        console.error("Erro ao buscar o CEP:", error.message);
+        return null;
     }
 }
 
-form.addEventListener("submit", handleSearch);
+// handle the submit form event
+async function handleSearch(event) {
+    event.preventDefault();
+    const cep = document.getElementById("cep").value;
+    const data = await fetchCep(cep);
+
+    if (!data) return;
+    fillCepAreas(data);
+}
+
+function fillCepAreas(data) {
+    logradouroArea.textContent = data.logradouro;
+    bairroArea.textContent = data.bairro;
+    localidadeArea.textContent = data.localidade;
+    estadoArea.textContent = data.estado;
+    regiaoArea.textContent = data.regiao;
+    ibgeArea.textContent = data.ibge;
+    dddArea.textContent = data.ddd;
+    ufArea.textContent = data.uf;
+}
+
+document.querySelector("form").addEventListener("submit", handleSearch);
